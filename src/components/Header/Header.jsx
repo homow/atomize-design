b0f477b8f9eaeaa0c34c3356290bc17270f9a295
homow <homowsh897@gmail.com>
2025-09-02 00:21:19 +0330

    initial coding

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,37 @@
 function Header () {
     return (
-        <header>
+        <header className={"container py-4 text-primary-gray font-medium text-sm"}>
+            {/* <=== header logo ===> */}
+            <div>
+                <a href="/">
+                    <svg className={"w-25 h-5"}>
+                        <use href={"#header-logo"}></use>
+                    </svg>
+                </a>
+            </div>
 
+            {/* <=== navigation header ===> */}
+            <nav>
+                <ul className={"*:hover:text-"}>
+                    <li>
+                        <svg className={"size-6"}>
+                            <use href={"#moon-icon"}></use>
+                        </svg>
+                    </li>
+                    <li>
+                        <a href="">Feature</a>
+                    </li>
+                    <li>
+                        <a href="">GitHub</a>
+                    </li>
+                    <li>
+                        <a href="">For Designers</a>
+                    </li>
+                    <li>
+                        <a href="">Documentation</a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
