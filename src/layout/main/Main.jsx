7f75a3bda085617d6b2efecf4597f787c76c64b0
homow <homowsh897@gmail.com>
2025-09-03 19:47:35 +0330

    create login from in HeroSection Component

--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -4,7 +4,7 @@ import HeroSection from '@/layout/main/hero-section/HeroSection.jsx';
 class Main extends Component {
     render () {
         return (
-            <main className={"pt-14"}>
+            <main className={"pt-14 mb-20"}>
                 {/* <=== hero section ===> */}
                 <HeroSection/>
             </main>
--- src/layout/main/hero-section/bottom-hero-section/BottomHeroSection.jsx
+++ src/layout/main/hero-section/bottom-hero-section/BottomHeroSection.jsx
@@ -7,12 +7,12 @@ class BottomHeroSection extends Component {
         return (
             <div className={"container mt-20"}>
                 {/* <=== user profile card ===> */}
-                <div className={""}>
+                <div>
                     <div className={"mb-8 space-x-2 text-center"}>
                         <CircleIconsBox icon={<svg className={"size-4.5"}>
                             <use href="#heart-icon"></use>
                         </svg>}/>
-                        <CircleIconsBox icon={<svg className={"size-4.5"}>
+                        <CircleIconsBox icon={<svg className={"fill-blue-500 size-4.5"}>
                             <use href="#Eye-icon"></use>
                         </svg>}/>
                         <CircleIconsBox icon={<svg className={"size-4.5"}>
@@ -31,9 +31,29 @@ class BottomHeroSection extends Component {
                     <UserProfileCard/>
                 </div>
 
-                {/* <=== hero section ===> */}
-                <div>
+                {/* <=== small login card ===> */}
+                <div className={"mt-10 shadow-rounded-box text-center p-8 space-y-14"}>
+                    <div className={"space-y-2"}>
+                        <p className={"text-xl font-medium tracking-wider"}>Login into your account</p>
+                        <p className={"text-gray-lt text-xs"}>Don`t have an account yet? <span className={"text-blue-500 font-medium cursor-pointer"}>Create New</span></p>
+                    </div>
 
+                    <div className={"flex flex-col gap-4"}>
+                        <label htmlFor={"mail-input"} className={"relative rounded-full pl-2 pr-8 py-2 border border-border-gray focus-within:border-blue-500 transition-all"}>
+                            <input placeholder={"mail@example.co"} id={"mail-input"} className={"w-full block peer outline-none grow text-sm xs:text-base placeholder:text-sm"} type="text"/>
+                            <svg className={"absolute top-1/2 -translate-1/2 right-0 size-5 fill-gray-lt"}>
+                                <use href="#mail-icon"></use>
+                            </svg>
+                        </label>
+                        <label htmlFor={"password-input"} className={"relative rounded-full pl-2 pr-8 py-2 border border-border-gray focus-within:border-blue-500 transition-all"}>
+                            <input placeholder={"Password"} id={"password-input"} className={"w-full block peer outline-none grow text-sm xs:text-base placeholder:text-sm"} type="text"/>
+                            <svg className={"absolute top-1/2 -translate-1/2 right-0 size-5 fill-gray-lt"}>
+                                <use href="#Eye-icon"></use>
+                            </svg>
+                        </label>
+                    </div>
+
+                    <button type={"button"} className={"btn-bg-blue block w-full"}>Login</button>
                 </div>
 
                 {/* <=== hero section ===> */}
