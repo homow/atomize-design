73b74a0313eed9eac5100ca90184597abc342765
homow <homowsh897@gmail.com>
2025-09-05 19:30:43 +0330

    create component for cricle border boxin advantage section

--- src/layout/main/react-advantage/ReactAdvantage.jsx
+++ src/layout/main/react-advantage/ReactAdvantage.jsx
@@ -1,5 +1,6 @@
 import {Component} from 'react';
 import AdvantageReactIcon from "@/layout/main/react-advantage/advantage-react-icon/AdvantageReactIcon.jsx";
+import ReactAdvantageCircleBorder from "@/layout/main/react-advantage/react-advantage-circle-border/ReactAdvantageCircleBorder.jsx";
 
 class ReactAdvantage extends Component {
     render() {
@@ -7,30 +8,9 @@ class ReactAdvantage extends Component {
             <section className={"pt-20"}>
                 <div className={"container"}>
                     {/* <=== circle borders box ===> */}
-                    <div className={"relative my-20 size-80"}>
-                        <div className={"*:absolute"}>
+                    <ReactAdvantageCircleBorder/>
 
-                            {/* <=== circle border ===> */}
-                            <div className={"w-full h-full -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-15"}></div>
-                            <div className={"w-8/10 h-8/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-25"}></div>
-                            <div className={"w-6/10 h-6/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-35"}></div>
-                            <div className={"w-4/10 h-4/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-45"}></div>
-
-                            {/* <=== react icon ===> */}
-                            <span className={"left-1/2 top-1/2 -translate-1/2 max-w-max"}>
-                                <svg className={"w-25 h-[6rem]"}>
-                                    <use href="#big-react-icon"></use>
-                                </svg>
-                            </span>
-
-                            <span className={"bg-orange-300 rounded-full p-2 top-20 left-8 shadow-primary"}>
-                                <svg className={"w-5 h-5 fill-primary-bg"}>
-                                    <use href="#plus-icon"></use>
-                                </svg>
-                            </span>
-                        </div>
-                    </div>
-
+                    {/* <=== react advantage info and icons ===> */}
                     <div>
                         {/* <=== info ===> */}
                         <h2 className={"font-bold text-3xl tracking-wider leading-10 md:text-4xl"}>
---
+++ src/layout/main/react-advantage/react-advantage-circle-border/ReactAdvantageCircleBorder.jsx
@@ -0,0 +1,33 @@
+import {Component} from 'react';
+
+class ReactAdvantageCircleBorder extends Component {
+    render() {
+        return (
+            <div className={"relative my-20 size-80"}>
+                <div className={"*:absolute"}>
+
+                    {/* <=== circle border ===> */}
+                    <div className={"w-full h-full -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-15"}></div>
+                    <div className={"w-8/10 h-8/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-25"}></div>
+                    <div className={"w-6/10 h-6/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-35"}></div>
+                    <div className={"w-4/10 h-4/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-45"}></div>
+
+                    {/* <=== react icon ===> */}
+                    <span className={"left-1/2 top-1/2 -translate-1/2 max-w-max"}>
+                                <svg className={"w-25 h-[6rem]"}>
+                                    <use href="#big-react-icon"></use>
+                                </svg>
+                            </span>
+
+                    <span className={"bg-orange-300 rounded-full p-2 top-20 left-8 shadow-primary"}>
+                                <svg className={"w-5 h-5 fill-primary-bg"}>
+                                    <use href="#plus-icon"></use>
+                                </svg>
+                            </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ReactAdvantageCircleBorder;
