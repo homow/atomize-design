40169dceeeb92367d5803bc7111215400cde9133
homow <homowsh897@gmail.com>
2025-09-05 19:27:35 +0330

    develope  circle border div

--- src/layout/main/react-advantage/ReactAdvantage.jsx
+++ src/layout/main/react-advantage/ReactAdvantage.jsx
@@ -8,13 +8,29 @@ class ReactAdvantage extends Component {
                 <div className={"container"}>
                     {/* <=== circle borders box ===> */}
                     <div className={"relative my-20 size-80"}>
-                        <div className={"*:absolute *:border-2 *:rounded-full *:p-4 *:inline-block *:top-1/2 *:left-1/2 *:-translate-1/2"}>
-                            <div className={"w-full h-full opacity-15"}></div>
-                            <div className={"w-8/10 h-8/10 opacity-25"}></div>
-                            <div className={"w-6/10 h-6/10 opacity-35"}></div>
-                            <div className={"w-4/10 h-4/10 opacity-45"}></div>
+                        <div className={"*:absolute"}>
+
+                            {/* <=== circle border ===> */}
+                            <div className={"w-full h-full -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-15"}></div>
+                            <div className={"w-8/10 h-8/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-25"}></div>
+                            <div className={"w-6/10 h-6/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-35"}></div>
+                            <div className={"w-4/10 h-4/10 -translate-1/2 rounded-full p-4 top-1/2 left-1/2 border-2 opacity-45"}></div>
+
+                            {/* <=== react icon ===> */}
+                            <span className={"left-1/2 top-1/2 -translate-1/2 max-w-max"}>
+                                <svg className={"w-25 h-[6rem]"}>
+                                    <use href="#big-react-icon"></use>
+                                </svg>
+                            </span>
+
+                            <span className={"bg-orange-300 rounded-full p-2 top-20 left-8 shadow-primary"}>
+                                <svg className={"w-5 h-5 fill-primary-bg"}>
+                                    <use href="#plus-icon"></use>
+                                </svg>
+                            </span>
                         </div>
                     </div>
+
                     <div>
                         {/* <=== info ===> */}
                         <h2 className={"font-bold text-3xl tracking-wider leading-10 md:text-4xl"}>
